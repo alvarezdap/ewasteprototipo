de2a796b75c4dfe8303f7796c4c07046dfa7e71a
alvarezdap <alvarezdap@pm.me>
2025-09-20 10:57:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ date = st.date_input("Seleccionar la fecha de recogida de los aparatos electrón
 def anadir_tipo_desecho():
     option_celular = st.selectbox('Celular',('Samsung','Iphone','Huawei','Xiaomi'))
     option_cantidad = st.selectbox('Cantidad', ('0-5','5-10','10-20'))
-    option_computadoras = st.selectbox('Computadoras',('Laptops','PCs'))
+    option_computadoras = st.selectbox('Computadoras',('Laptops','PCs'), id=1)
     option_computadora = st.selectbox('Cantidad', ('0-5','5-10','10-20'))
 
 def calendarizar():
