174e2090d566d20b131d48363eb96b4d7f124df5
alvarezdap <alvarezdap@pm.me>
2025-09-20 10:21:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,14 @@ def anadir_tipo_desecho():
     option = st.selectbox('Celular',('Samsung','Iphone','Huawei','Xiaomi'))
 
 def calendarizar():
-    st.button("Calendarizar recogida de desechos")
-    st.balloons()
+    st.form_submit_button(label="Calendarizar recogida de desechos", help=None, on_click=st.ballons, args=None, kwargs=None, *, key=None, type="secondary", icon=None, disabled=False, use_container_width=None, width="content")
+
+st.map()
 
 anadir_tipo_desecho()
 calendarizar()
 
-st.map()
+
 
 
 
