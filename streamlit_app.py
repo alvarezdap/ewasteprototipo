d93fd0875f40c7bcd90a8e1b84ad53ea3a221f61
alvarezdap <alvarezdap@pm.me>
2025-09-20 09:52:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ st.title('Desechos tecnológicos')
 date = st.date_input("Seleccionar la fecha de recogida de los aparatos electrónicos")
 
 def anadir_tipo_desecho():
-    option = st.selectbox('Celular',('Samsung','Iphone','Huawei','Xiaomi'),'Computadoras',('Apple','PCs','Laptops'))
+    option = st.selectbox('Celular',('Samsung','Iphone','Huawei','Xiaomi'))
 
 anadir_tipo_desecho()
