41bd6b707e3ef8a0bc75b8a121805697e0d35608
alvarezdap <alvarezdap@pm.me>
2025-09-20 10:32:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+*import streamlit as st
 
 st.title('Desechos tecnológicos')
 
@@ -9,7 +9,7 @@ def anadir_tipo_desecho():
     option_cantidad = st.selectbox('Cantidad', ('0-5','5-10','10-20'))
 
 def calendarizar():
-    st.button(label="Calendarizar recogida de desechos", help=None, on_click=st.ballons, args=None, kwargs=None, key=None, type="secondary", icon=None, disabled=False, use_container_width=None, width="content")
+    st.button(label="Calendarizar recogida de desechos", help=None, on_click=st.balloons, args=None, kwargs=None, key=None, type="secondary", icon=None, disabled=False, use_container_width=None, width="content")
 
 st.map()
 
