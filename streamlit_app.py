91e7ede14ec18d6e3f4124ee9a93d6624e84c38c
alvarezdap <alvarezdap@pm.me>
2025-09-20 11:12:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,8 @@ st.title('Desechos tecnológicos')
 date = st.date_input("Seleccionar la fecha de recogida de los aparatos electrónicos")
 
 def anadir_tipo_desecho():
-    option_celular = st.selectbox('Celular',('Samsung','Iphone','Huawei','Xiaomi'), index=1)
+    option_celular = st.selectbox('Celular',('Samsung','Iphone','Huawei','Xiaomi'))
     option_cantidad = st.selectbox('Cantidad', ('0-5','5-10','10-20'))
-    option_computadoras = st.selectbox('Computadoras',('Laptops','PCs'), index=2)
-    option_computadora = st.selectbox('Cantidad', ('0-5','5-10','10-20'))
 
 def calendarizar():
     st.button(label="Calendarizar recogida de desechos", help=None, on_click=st.balloons, args=None, kwargs=None, key=None, type="secondary", icon=None, disabled=False, use_container_width=None, width="content")
